7dc2592a8f923b0173411ff9c20701ae2db2be58
gitpratap <mbhanupratap720@gmail.com>
2024-01-22 20:22:12 -0500

    Added Successfully

---
+++ HelloWorld/src/p1/Demo1.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class Demo1 {
+	static boolean m1() {
+		for(boolean a=true;a=true;) {
+			System.out.println(a);
+			break;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {		
+		Demo1.m1();
+	}
+}
